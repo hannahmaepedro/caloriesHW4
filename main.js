dc49ffa065e82ed7e8f762e371265eb4b42f81c5
hannahmaepedro <101531444+hannahmaepedro@users.noreply.github.com>
2022-05-02 22:40:20 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,23 +1,64 @@
-calories = [];
+document.addEventListener("DOMContentLoaded", function (event) {
+
+    document.getElementById("submitCalories").addEventListener("click", function () {
+            (document.getElementById("foodName").value,
+            document.getElementById("numberOfCalories").value);
+        console.log(FoodItemArray)
+        
+    });
+
+    // document.getElementById("totalCalories").addEventListener("click", function() {
+    //     console.log(FoodItemArray);
+    //     // for (i = 0; i < someName.length; i++) {
+    //     //     if (document.getElementById("foodName").value == FoodItemArray[i].someName){
+                
+    //     //     }
+    //     // }
+    // })
+});
+
+FoodItemArray = [];
 
 //constructor
 let someName = function(pName, pCalories){
     this.someName = parseInt(pName);
     this.someCalories = parseInt(pCalories);
+    this.calculateCal = function () {
+        for (let i in someCalories)
+        totalCalories += someCalories [i]
+    }
+
+    let newSomeName1 = {
+        someName: "banana",
+        someCalories: 20,
+    }
+
+    let newSomeName2 = new someName2("hot dog", 450 );
+
+    FoodItemArray.push(newSomeName1);
+    FoodItemArray.push(newSomeName2);
+    console.log(someName)
+
+    document.getElementById("foodName").value = "",
+    document.getElementById("numberOfCalories").value = ""
 }
 
-calories.push(new someName ("banana", 20));
-calories.push(new someName ("hot dog", 450));
+// function submitCalories() {
+    // let newSomeName1 = {
+    //     someName: "banana",
+    //     someCalories: 20,
+    // }
 
+    // let newSomeName2 = new someName("hot dog", 450 );
 
-document.addEventListener("DOMContentLoaded", function (event) {
+    // FoodItemArray.push(newSomeName1);
+    // FoodItemArray.push(newSomeName2);
+    // console.log(FoodItemArray)
 
-    document.getElementById("submitCalories").addEventListener("click", function () {
-        let xName = document.getElementById("foodName").value;
-        let xCalories = document.getElementById("numberOfCalories").value;
-        let totalCalories = new cal(xName, xCalories)
-        calories.push(totalCalories);
-        console.log(calories);
-    });
-});
+    // document.getElementById("foodName").value = "",
+    // document.getElementById("numberOfCalories").value = ""
+// }
 
+// function totalCalories() {
+    
+// }
